6258ef91d293ed09f5a46af9fcb3783879a415db
Dariusz Uchman <dariusz.uchman@snowsoftware.com>
2023-02-15 12:08:36 +0100

    Power Tools: Concurrency: Grep: version 2,wait and process using select

---
+++ powertools/grep2/job.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"regexp"
+)
+
+type Job struct {
+	filename string
+	results  chan<- Result
+}
+
+func (job *Job) Do(lineRx *regexp.Regexp) {
+	file, err := os.Open(job.filename)
+	if err != nil {
+		log.Printf("error: %s\n", err)
+		return
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	for lino := 1; ; lino++ {
+		line, err := reader.ReadBytes('\n')
+		line = bytes.TrimRight(line, "\n\r")
+		if lineRx.Match(line) {
+			job.results <- Result{job.filename, lino, string(line)}
+		}
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("error:%d: %s\n", lino, err)
+			}
+			break
+		}
+	}
+}
---
+++ powertools/grep2/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"go-concurrency/powertools/utils"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"runtime"
+)
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	if len(os.Args) < 3 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Printf("usage: %s <regexp> <files>\n",
+			filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+
+	if lineRx, err := regexp.Compile(os.Args[1]); err != nil {
+		log.Fatalf("invalid regexp: %s\n", err)
+	} else {
+		grep(lineRx, utils.CommandLineFiles(os.Args[2:]))
+	}
+}
