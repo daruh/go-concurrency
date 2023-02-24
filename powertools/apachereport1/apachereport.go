e2a4ae1fa34d42006133fdb0cf3d99ff979d563a
Dariusz Uchman <dariusz.uchman@snowsoftware.com>
2023-02-24 09:23:06 +0100

    Power Tools: Concurrency: Apache Report: implementation 1

--- powertools/apachereport1/apachereport.go
+++ powertools/apachereport1/apachereport.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"go-concurrency/powertools/safemap"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 	"runtime"
 )
 
-//go run .\apachereport.go ..\logsData\access_test.log
+//go run .\apachereport.go ../logsData/access_test.log
 
 var workers = runtime.NumCPU()
 
@@ -22,25 +26,66 @@ func main() {
 	done := make(chan struct{}, workers)
 	pageMap := safemap.New()
 
-	go readLines(os.Args[1], lines)
+	path := os.Args[1]
+	go readLines(path, lines)
 	processLines(done, pageMap, lines)
 	waitUntil(done)
 	showResults(pageMap)
-
 }
 
 func readLines(filename string, lines chan<- string) {
 
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal("failed to open the file:", err)
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		if line != "" {
+			lines <- line
+		}
+		if err != nil {
+			if err != io.EOF {
+				log.Println("failed to finish reading the file:", err)
+			}
+			break
+		}
+	}
+	close(lines)
 }
 
 func processLines(done chan<- struct{}, pageMap safemap.SafeMap, lines <-chan string) {
+	getRx := regexp.MustCompile(`GET[ \t]+([^ \t\n]+[.]php?)`)
+	incrementer := func(value interface{}, found bool) interface{} {
+		if found {
+			return value.(int) + 1
+		}
+		return 1
+	}
 
+	for i := 0; i < workers; i++ {
+		go func() {
+			for line := range lines {
+				if matches := getRx.FindStringSubmatch(line); matches != nil {
+					pageMap.Update(matches[1], incrementer)
+				}
+			}
+			done <- struct{}{}
+		}()
+	}
 }
 
 func waitUntil(done <-chan struct{}) {
-
+	for i := 0; i < workers; i++ {
+		<-done
+	}
 }
 
 func showResults(pageMap safemap.SafeMap) {
-
+	pages := pageMap.Close()
+	for page, count := range pages {
+		fmt.Printf("%8d %s\n", count, page)
+	}
 }
